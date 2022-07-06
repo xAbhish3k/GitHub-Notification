0e9f0a8f2a89f971aa7682fcfb7d1fe5f1645622
Abhishek singh <84198635+xAbhish3k@users.noreply.github.com>
2022-07-07 04:54:49 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,11 @@
+# Your Bot token in the quotes taken from @BotFather on Telegram
+
+BOT_TOKEN = "5020504303:thewarbotz"
+
+# Project Name of your Project
+
+PROJECT_NAME = "GitHub-Notification"
+
+# If you forked the Repository, grab your repo's link and paste it below!
+
+GIT_REPO_URL = "https://github.com/xAbhish3k/GitHub-Notification"
